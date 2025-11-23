78075e185b914c979a0efa85296cfc7b9846eb34
Lady-Byron <samantha-everett@outlook.com>
2025-11-23 16:35:53 -0500

    Update index.ts

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,23 +1,9 @@
-import app from 'flarum/forum/app';
-import Widgets from 'flarum/extensions/afrux-forum-widgets-core/common/extend/Widgets';
+// js/src/forum/index.ts
 
-import TrendsWidget from './components/TrendsWidget';
+import app from 'flarum/forum/app';
+import registerWidget from '../common/register';
 import { extName } from '../common/extName';
 
 app.initializers.add(extName, () => {
-  // 权限检查：没有“查看论坛”权限就不要注册组件
-  if (!app.forum.attribute('canViewForum')) {
-    return;
-  }
-
-  new Widgets()
-    .add({
-      key: 'liplum-trends-widget',
-      component: TrendsWidget,
-      isDisabled: false,
-      isUnique: true,
-      placement: 'top',
-      position: 1,
-    })
-    .extend(app, extName);
+  registerWidget(app);
 });
